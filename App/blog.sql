a601db969de5ff104abefec3ebceba4258f420bf
lykich <lykich@lweb.pl.ua>
2015-03-18 10:12:55 +0200

    Add pagination and improve show data from hash(sort,reverse)

--- App/blog.sql
+++ App/blog.sql
@@ -25,7 +25,7 @@ DROP TABLE IF EXISTS `blog`;
 CREATE TABLE `blog` (
   `id` int(10) NOT NULL,
   `h1` char(20) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `img_link` char(20) NOT NULL,
+  `img_link` char(40) NOT NULL,
   `date` datetime NOT NULL,
   `small_post` varchar(255) NOT NULL,
   `big_post` text NOT NULL,
@@ -41,8 +41,33 @@ CREATE TABLE `blog` (
 
 LOCK TABLES `blog` WRITE;
 /*!40000 ALTER TABLE `blog` DISABLE KEYS */;
-INSERT INTO `blog` VALUES (1,'Start developing','/img/blog/2.jpg','2015-02-18 11:04:23','Start develping my blog and ...','Start develping my blog and ...',1),(2,'Add database','/img/blog/1.jpg','2015-02-18 13:40:54','Today add connect to database and save data for it','Today add connect to database and save data for post in it',1);
+INSERT INTO `blog` VALUES (1,'Start developing','/img/blog/2.jpg','2015-02-18 11:04:23','Start develping my blog and ...','Start develping my blog and ...',1),(2,'Add database','/img/blog/1.jpg','2015-02-18 13:40:54','Today add connect to database and save data for it','Today add connect to database and save data for post in it',1),(3,'Think','/img/blog/cat_think.jpg','2015-03-03 21:14:00','Today when I went to home','Today when I went to home I thought. Then more I  make commit to git then more learn english. :)',2),(4,'AC/DC, MySQL','/img/blog/2015-03-04-151410.jpg','2015-03-04 17:46:51','Today my working day started for me  with AC/DC','\nToday my working day started for me  with AC/DC and task with MySQL.<br> Task next get first day and last day of month have March 2015. Why do this ? <br>I have to show data from archive in blog  for  year and month. <br>Use functions the MySQL it is easier. <br> <img alt=\"mysql last day of month and first day\" src=\"/img/blog/mysql.png\">',1),(5,'Winter goodbye.','/img/blog/bord_skis_on_cupboard.jpg','2015-03-16 17:02:06','Yesterday,I have put on the wardrobe my snowboard. Winter goodbye.','Yesterday,I have put on the wardrobe my snowboard. Winter goodbye.\r\nBut, today called me my ski friend with Bukovel and told me, what now in Bukovel more snow and  little people, the best time for ski and snowbord. For me winter goodbye.',2);
 /*!40000 ALTER TABLE `blog` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `categories`
+--
+
+DROP TABLE IF EXISTS `categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `categories` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `categories_name` char(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `categories_name` (`categories_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categories`
+--
+
+LOCK TABLES `categories` WRITE;
+/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+INSERT INTO `categories` VALUES (1,'Develop'),(2,'Life');
+/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -54,4 +79,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-03-03 16:32:29
+-- Dump completed on 2015-03-18 10:04:25
--- App/lib/App.pm
+++ App/lib/App.pm
@@ -3,6 +3,7 @@ use Dancer2;
 use Dancer2::Plugin::Database;
 use Data::Page;
 use Dancer2::Logger::Console;
+use Data::Pageset::Render;
 use Data::Dumper;
 
 our $VERSION = '0.1';
@@ -22,27 +23,49 @@ get '/' => sub {
 };
 
 get '/blog' => sub {
-	
-    my $sth = database->prepare( 'SELECT * FROM blog limit 4' );
+    my $sth = database->prepare( 'SELECT * FROM blog order by date DESC limit 4' );
 	   $sth->execute() or die $DBI::errstr;
     my $pg = database->prepare( 'select count(*) from blog' );
        $pg->execute() or die $DBI::errstr;
     my ($pg_count) = $pg->fetchrow_array;
-    my $carent_page = '1';
-    my $page = Data::Page->new( $pg_count, 4, $carent_page );
-    	
-    #print "MAPPING:" . Dumper($page);	
-    #print "Entries:", $page->total_entries, "\n";
-    #print "Pages range to: ", $page->last_page, "\n";
-    #print "Pages range to: ", $page->last_page, "\n";
-    	
+    my $carent_page = '1';    
+    my $pager = get_paginator( $pg_count,4,1);
+    my $href_p = "./blog/page/";	
     template 'index', {
 	  'css_active' => 'blog',
 	  'breadcrumb' => 'Blog',	
 	  'rows'       => $sth->fetchall_hashref('id'),
-	  'c_page'     => $page->current_page,
-	  'pg_count'   => $pg_count
+	  'pager'      => $pager,
+	  'href_p'     => $href_p
     }
+};
+
+get '/blog/page/:name' => sub {
+    my $param_page = params->{name};   
+    my $step_p;
+    if( $param_page != 1 ) {
+        $step_p = ( $param_page - 1 )*4;
+     } else {
+		$step_p = 1;
+	 }	 
+    my $sth = database->prepare( "SELECT * FROM blog WHERE id >= '$step_p' ORDER BY date limit 4" );
+       $sth->execute() or die $DBI::errstr;
+    my $pg = database->prepare( 'select count(*) from blog' );
+       $pg->execute() or die $DBI::errstr;
+    my ($pg_count) = $pg->fetchrow_array;
+    my $carent_page = '1';    
+    my $pager = get_paginator( $pg_count,4,$param_page);
+    my $href_p = "";
+    my $h_b = "blog/";
+    template 'index', {
+	  'css_active' => 'blog',
+	  'breadcrumb' => 'Blog',	
+	  'rows'       => $sth->fetchall_hashref('id'),
+	  'pager'      => $pager,
+	  'hb'         => $h_b,
+	  'href_p'     => $href_p
+
+    }   
 };
 
 get '/blog/:name' => sub {
@@ -51,11 +74,11 @@ get '/blog/:name' => sub {
 	    $post->execute() or die $DBI::errstr;
      my $cat = database->prepare( "SELECT * FROM categories" );
         $cat->execute() or die $DBI::errstr;
-     my $post4 = database->prepare( "SELECT h1,date FROM blog ORDER by date limit 4" );  
+     my $post4 = database->prepare( "SELECT h1,date FROM blog ORDER by date DESC limit 4" );  
         $post4->execute() or die $DBI::errstr;
      my $p_ar = database->prepare( "SELECT DISTINCT DATE_FORMAT(date,'%M %Y') as ym FROM blog order by date limit 12" );
         $p_ar->execute() or die $DBI::errstr;
-        
+     my $href_b = "blog/";   
 	 template 'index', {
 	  'css_active' => 'blog',
 	  'breadcrumb' => 'Blog post',
@@ -63,6 +86,7 @@ get '/blog/:name' => sub {
 	  'rows_c'     => $cat->fetchall_hashref('id'), 
 	  'rows_post4' => $post4->fetchall_hashref('h1'),
 	  'rows_ar'    => $p_ar->fetchall_hashref('ym'),
+	  'href_b'     => $href_b
      }		    
 	    
 };
@@ -70,7 +94,7 @@ get '/blog/:name' => sub {
 
 get '/blog/categories/:name' => sub {
     my $post_cat = params->{name};
-    my $p_cat = database->prepare( "SELECT h1,img_link,date,small_post,big_post FROM blog as b,categories as c WHERE b.categories_id=c.id AND c.categories_name='$post_cat' ORDER by date limit 4" );
+    my $p_cat = database->prepare( "SELECT h1,img_link,date,small_post,big_post FROM blog as b,categories as c WHERE b.categories_id=c.id AND c.categories_name='$post_cat' ORDER by date DESC limit 4" );
        $p_cat->execute() or die $DBI::errstr;
     template 'index', {
 	  'css_active' => 'blog',
@@ -82,7 +106,7 @@ get '/blog/categories/:name' => sub {
 
 get '/blog/archive/:name' => sub {
     my $post_arch = params->{name};    
-    my $s_arch = database->prepare( "SELECT * FROM blog WHERE date BETWEEN DATE_FORMAT((STR_TO_DATE('$post_arch','%M %Y')),'%Y-%m-01 00:00:00') AND DATE_FORMAT(LAST_DAY(DATE_FORMAT((STR_TO_DATE('$post_arch','%M %Y')),'%Y-%m-01 00:00:00')),'%Y-%m-%d 23:59:59') order by date limit 4" );
+    my $s_arch = database->prepare( "SELECT * FROM blog WHERE date BETWEEN DATE_FORMAT((STR_TO_DATE('$post_arch','%M %Y')),'%Y-%m-01 00:00:00') AND DATE_FORMAT(LAST_DAY(DATE_FORMAT((STR_TO_DATE('$post_arch','%M %Y')),'%Y-%m-01 00:00:00')),'%Y-%m-%d 23:59:59') ORDER by date DESC limit 4" );
        $s_arch->execute() or die $DBI::errstr;
        
     template 'index', {
@@ -102,6 +126,22 @@ get '/contact' => sub {
 		
    }
 };
+
+sub get_paginator {
+
+    my ( $pg_count,$ent_p,$curr_p ) = @_; 
+    my $pager = Data::Pageset::Render->new( {
+       total_entries    => $pg_count,
+       entries_per_page => $ent_p,
+       current_page     => $curr_p,
+       pages_per_set    => 5,
+       mode             => 'slider'
+       });
+
+    return $pager;
+
+}
+
 
 
 true;
